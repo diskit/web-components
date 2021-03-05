f23ce5d10ece4ec0a0cd27cb0475ad96eede952b
Daisuke Itakura <daisuke.itakura@uzabase.com>
2021-03-05 20:07:40 +0900

    add token

---
+++ apps/token/src/test/kotlin/disk/token/TokenApplicationTests.kt
@@ -0,0 +1,13 @@
+package disk.token
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class TokenApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
