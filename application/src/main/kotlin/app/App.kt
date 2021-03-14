64eb0d983812f3f87f34b40e6a81cae194e57bc8
Daisuke Itakura <daisuke.itakura@uzabase.com>
2021-03-15 07:58:28 +0900

    add html

--- application/src/main/kotlin/app/App.kt
+++ application/src/main/kotlin/app/App.kt
@@ -1,6 +1,7 @@
 package app
 
 import io.jooby.Kooby
+import io.jooby.ModelAndView
 import io.jooby.runApp
 import io.jooby.thymeleaf.ThymeleafModule
 
@@ -9,7 +10,12 @@ class App: Kooby({
   install(ThymeleafModule())
 
   get("/") {
-    "Welcome to Jooby!"
+    val scriptLocation =
+      "${environment.config.getString("lib.components.endpoint")}?${environment.config.getString("lib.components.accessKey")}=token"
+
+    ModelAndView("index.html")
+      .put("scriptLocation", scriptLocation)
+      .put("name", "hoge")
   }
 })
 
