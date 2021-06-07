8d095eaadfaf6e451e9c6e185a3ef5ad5e6fdc02
Daisuke Itakura <daisuke.itakura@uzabase.com>
2021-06-08 08:35:53 +0900

    add plugin

--- apps/gateway/docker/run.sh
+++ apps/gateway/docker/run.sh
@@ -2,10 +2,11 @@
 
 cwd=$(cd $(dirname $0); pwd)
 
-docker run --rm -it \
+docker run --rm -d \
   -e KONG_DECLARATIVE_CONFIG="/etc/kong-nginx/kong.yml" \
   -e KONG_DATABASE="off" \
   -v $PWD/kong.yml:/etc/kong-nginx/kong.yml \
   -v $PWD/kong.conf:/etc/kong/kong.conf \
+  -p 8080:8000 \
   --name kong-gateway \
-  kong:2.4.0-alpine
+  kong:2.4.0-ubuntu
