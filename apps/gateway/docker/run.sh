84b0c457dd3de6883a15fe57e4a280b6789864c9
diskit <diskit.1980@gmail.com>
2021-06-14 20:41:59 +0900

    Split cookie settings and requests

--- apps/gateway/docker/run.sh
+++ apps/gateway/docker/run.sh
@@ -7,6 +7,7 @@ docker rm -f kong-gateway
 docker run -d \
   -e KONG_DECLARATIVE_CONFIG="/etc/kong-nginx/kong.yml" \
   -e KONG_DATABASE="off" \
+  -e AUTH_ENDPOINT="http://host.docker.internal:8083" \
   -v $PWD/kong.yml:/etc/kong-nginx/kong.yml \
   -v $PWD/kong.conf:/etc/kong/kong.conf \
   -p 8080:8000 \
--- apps/gateway/plugins/token-upgrader/kong/plugins/token-upgrader/handler.lua
+++ apps/gateway/plugins/token-upgrader/kong/plugins/token-upgrader/handler.lua
@@ -1,3 +1,7 @@
+local http = require "resty.http"
+local json = require "json"
+local auth_endpoint = os.getenv('AUTH_ENDPOINT')
+
 local plugin = {
   PRIORITY = 1000, -- set the plugin priority, which determines plugin execution order
   VERSION = "0.1",
@@ -8,13 +12,27 @@ function plugin:init_worker()
 end
 
 function plugin:access(plugin_conf)
-
-end 
-
-
-function plugin:header_filter(plugin_conf)
-  kong.log('** header_filter **')
+  local token = kong.request.get_query_arg("token")
+  local client = http.new()
+  local res, err = client:request_uri(auth_endpoint .. "/credentials/" .. token, {
+    method = "GET",
+    ssl_verify = false
+  })
+  if not(res) or res.status ~= 200 then
+    return
+  end
+  local json = json.decode(res.body)
+  local credential = json.credential
+  kong.ctx.plugin.credential = credential
 end
 
+function plugin:header_filter(plugin_conf)
+  local credential = kong.ctx.plugin.credential
+  local status = kong.response.get_status()
+  if not credential or status ~= 200 then
+    return
+  end
+  kong.response.add_header("set-cookie", "t=" .. credential .. "; HttpOnly;")
+end
 
 return plugin
