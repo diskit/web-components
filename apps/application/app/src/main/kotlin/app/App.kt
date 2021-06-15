878ad3dc8e629e1e15cb78370e94d49b89776834
diskit <diskit.1980@gmail.com>
2021-06-16 08:37:06 +0900

    add log

--- apps/application/app/src/main/kotlin/app/App.kt
+++ apps/application/app/src/main/kotlin/app/App.kt
@@ -1,18 +1,42 @@
 package app
 
+import com.fasterxml.jackson.databind.ObjectMapper
+import com.fasterxml.jackson.module.kotlin.KotlinModule
+import com.fasterxml.jackson.module.kotlin.readValue
 import io.jooby.Kooby
 import io.jooby.ModelAndView
 import io.jooby.runApp
 import io.jooby.thymeleaf.ThymeleafModule
+import java.net.URI
+import java.net.http.HttpClient
+import java.net.http.HttpRequest
+import java.net.http.HttpResponse
+import java.nio.charset.Charset
 
 class App: Kooby({
     install(ThymeleafModule())
 
+    val httpClient = HttpClient.newBuilder().build()
+    val mapper = ObjectMapper().registerModule(KotlinModule())
+    val authEndpoint = environment.config.getString("auth.endpoint")
+    val resourceEndpoint = environment.config.getString("resource.endpoint")
+
     get("") {
+        val token = HttpRequest.newBuilder(URI.create(authEndpoint))
+            .POST(HttpRequest.BodyPublishers.ofString("", Charset.forName("UTF-8")))
+            .header("content-type", "application/json")
+            .build()
+            .let { httpClient.send(it, HttpResponse.BodyHandlers.ofString()) }
+            .let { it.body() }
+            .let { mapper.readValue<TokenResponseJson>(it) }
+
+
         ModelAndView("index.html")
-            .put("scriptLocation", "/scripts/app.js")
+            .put("scriptLocation", "$resourceEndpoint?token=${token.token}")
     }
 })
+
+data class TokenResponseJson(val token: String)
 
 fun main(args: Array<String>) {
     runApp(args, App::class)
