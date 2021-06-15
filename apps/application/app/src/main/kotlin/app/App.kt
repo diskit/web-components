0a8e893781b0c4d72d76741e0c84bd1328a70f90
diskit <diskit.1980@gmail.com>
2021-06-16 07:40:48 +0900

    add view

--- apps/application/app/src/main/kotlin/app/App.kt
+++ apps/application/app/src/main/kotlin/app/App.kt
@@ -1,11 +1,17 @@
 package app
 
 import io.jooby.Kooby
+import io.jooby.ModelAndView
 import io.jooby.runApp
 import io.jooby.thymeleaf.ThymeleafModule
 
 class App: Kooby({
     install(ThymeleafModule())
+
+    get("") {
+        ModelAndView("index.html")
+            .put("scriptLocation", "/scripts/app.js")
+    }
 })
 
 fun main(args: Array<String>) {
