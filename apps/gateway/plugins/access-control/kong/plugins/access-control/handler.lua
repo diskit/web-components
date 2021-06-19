7c46debf61106999b8a656b5e85b75f48b1bab1f
diskit <diskit.1980@gmail.com>
2021-06-19 19:57:29 +0900

    decode credential

--- apps/gateway/plugins/access-control/kong/plugins/access-control/handler.lua
+++ apps/gateway/plugins/access-control/kong/plugins/access-control/handler.lua
@@ -1,3 +1,7 @@
+local http = require "resty.http"
+local json = require "json"
+local auth_endpoint = os.getenv('AUTH_ENDPOINT')
+
 local plugin = {
   PRIORITY = 1000,
   VERSION = "0.1.0",
@@ -8,11 +12,29 @@ function plugin:init_worker()
 end
 
 function plugin:access(plugin_conf)
-  kong.log("** access start **")
   local token = ngx.var.cookie_t
-  kong.log("token=", token)
-  -- kong.service.request.set_header(plugin_conf.request_header, "this is on a request")
-  kong.log("** access end **")
+  if not(token) then
+    return
+  end
+  local client = http.new()
+  local request_json = {
+    ["credential"] = token
+  }
+  local res, err = client:request_uri(auth_endpoint .. "/authn", {
+    method = "POST",
+    headers = {
+      ["content-type"] = "application/json;"
+    },
+    body = json.encode(request_json),
+    ssl_verify = false
+  })
+  if not(res) or res.status ~= 200 then
+    return
+  end
+  local json = json.decode(res.body)
+  kong.service.request.set_header("x-user-id", json.user)
+  kong.service.request.set_header("x-consumer-app", json.consumer)
+  kong.service.request.clear_header("cookie")
 end
 
 return plugin
