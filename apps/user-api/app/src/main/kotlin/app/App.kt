228b4e8540d42ad663f1b0d8ea500f6eb7fe365e
diskit <diskit.1980@gmail.com>
2021-06-19 19:33:12 +0900

    optimize imports

--- apps/user-api/app/src/main/kotlin/app/App.kt
+++ apps/user-api/app/src/main/kotlin/app/App.kt
@@ -2,17 +2,10 @@ package app
 
 import com.fasterxml.jackson.databind.ObjectMapper
 import com.fasterxml.jackson.module.kotlin.KotlinModule
-import com.fasterxml.jackson.module.kotlin.readValue
 import io.jooby.Kooby
-import io.jooby.ModelAndView
 import io.jooby.json.JacksonModule
 import io.jooby.runApp
-import io.jooby.thymeleaf.ThymeleafModule
-import java.net.URI
-import java.net.http.HttpClient
-import java.net.http.HttpRequest
-import java.net.http.HttpResponse
-import java.nio.charset.Charset
+
 
 class App: Kooby({
     install(JacksonModule(ObjectMapper().registerModule(KotlinModule())))
